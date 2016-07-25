7656e94519a44fe56fc19a43d3c698eba7d6e286
mauricio duque orozco <mauricio.duque.eje@icloud.com>
2016-07-25 16:25:45 -0500

    prueba

--- serverIO/index.js
+++ serverIO/index.js
@@ -22,8 +22,8 @@ module.exports = function(http){
 console.log(http);
   var address = http.address || ip
 
-  var socketIO = io.connect(address, {reconnect:true})
-  //var socketIO = io.connect('http://190.15.141.74:2000', {reconnect:true})
+  //var socketIO = io.connect(address, {reconnect:true})
+  var socketIO = io.connect('http://190.15.141.74:8000', {reconnect:true})
 
   socketIO.on('connect', function(){
     console.log('connected');
